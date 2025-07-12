3b4527f38bca147c154f2198932d5ddde5d43113
naruto777549 <shekhikrar026@gmail.com>
2025-07-12 18:48:29 +0530

    Update db.py

--- revengers/db.py
+++ revengers/db.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URL
 
 client = AsyncIOMotorClient(MONGO_URL)
-db = client["BlackGameBot"]
+db = client["revengers"]
 
 Users = db["users"] 
 users = Users 
