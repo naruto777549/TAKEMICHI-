022295538eee9b0d96eb72e172f0321fd995268d
naruto777549 <shekhikrar026@gmail.com>
2025-07-13 05:47:52 +0530

    Update help.py

--- revengers/Modules/help.py
+++ revengers/Modules/help.py
@@ -35,3 +35,40 @@ async def help_command(bot, message: Message):
         caption=caption,
         reply_markup=buttons
     )
+
+@bot.on_callback_query(filters.regex("help_menu"))
+async def help_menu_callback(bot, query: CallbackQuery):
+    await query.answer()
+
+    user_mention = query.from_user.mention
+    image_url = "https://files.catbox.moe/wggk0a.jpg"
+
+    caption = f"""
+⁉️ Hᴇʏ...!! {user_mention} ~
+
+```➪ I ᴀᴍ ᴀ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇ sʜᴀʀɪɴɢ ʙᴏᴛ, ᴍᴇᴀɴᴛ ᴛᴏ ᴘʀᴏᴠɪᴅᴇ ғɪʟᴇs ᴀɴᴅ ɴᴇᴄᴇssᴀʀʏ sᴛᴜғғ ᴛʜʀᴏᴜɢʜ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ ғᴏʀ sᴘᴇᴄɪғɪᴄ ᴄʜᴀɴɴᴇʟs.
+
+➪ Iɴ ᴏʀᴅᴇʀ ᴛᴏ ɢᴇᴛ ᴛʜᴇ ғɪʟᴇs ʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ ᴀʟʟ ᴍᴇɴᴛɪᴏɴᴇᴅ ᴄʜᴀɴɴᴇʟ ᴛʜᴀᴛ ɪ ᴘʀᴏᴠɪᴅᴇ ʏᴏᴜ ᴛᴏ ᴊᴏɪɴ. Yᴏᴜ ᴄᴀɴ ɴᴏᴛ ᴀᴄᴄᴇss ᴏʀ ɢᴇᴛ ᴛʜᴇ ғɪʟᴇs ᴜɴʟᴇss ʏᴏᴜ ᴊᴏɪɴᴇᴅ ᴀʟʟ ᴄʜᴀɴɴᴇʟs.
+
+➪ Sᴏ ᴊᴏɪɴ Mᴇɴᴛɪᴏɴᴇᴅ Cʜᴀɴɴᴇʟs ᴛᴏ ɢᴇᴛ Fɪʟᴇs ᴏʀ ɪɴɪᴛɪᴀᴛᴇ ᴍᴇssᴀɢᴇs...```
+
+‣ /help - Oᴘᴇɴ ᴛʜɪs ʜᴇʟᴘ ᴍᴇssᴀɢᴇ !
+◈ Sᴛɪʟʟ ʜᴀᴠᴇ ᴅᴏᴜʙᴛs, ᴄᴏɴᴛᴀᴄᴛ ʙᴇʟᴏᴡ ᴘᴇʀsᴏɴs/ɢʀᴏᴜᴘ ᴀs ᴘᴇʀ ʏᴏᴜʀ ɴᴇᴇᴅ !
+"""
+
+    buttons = InlineKeyboardMarkup([
+        [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ɢʀᴏᴜᴘ", url="https://t.me/+ZyRZJntl2FU0NTk1")],
+        [
+            InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/Uzumaki_X_Naruto_6"),
+            InlineKeyboardButton("ᴅᴇᴠʟᴏᴘᴇʀ", url="https://t.me/Uzumaki_X_Naruto_6")
+        ],
+        [InlineKeyboardButton("🔙 ʙᴀᴄᴋ", callback_data="back_menu")]
+    ])
+
+    await query.message.delete()
+    await bot.send_photo(
+        chat_id=query.message.chat.id,
+        photo=image_url,
+        caption=caption,
+        reply_markup=buttons
+    )
