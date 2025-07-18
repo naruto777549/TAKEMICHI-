0ccc6c7cbc923371236a0696971d43943c635ed8
naruto777549 <shekhikrar026@gmail.com>
2025-07-18 06:20:45 +0530

    Update genlink.py

--- revengers/Modules/genlink.py
+++ revengers/Modules/genlink.py
@@ -1,14 +1,15 @@
+import random
+import string
 from pyrogram import filters
-from pyrogram.types import (
-    Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-)
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
 from revengers import bot
-from revengers.db import file_collection, Users
+from revengers.db import file_collection, Banned
 
+BOT_USERNAME = "UzumakiFileHavenbot"
 CHANNELS = [
-    ("https://t.me/Bey_war_updates", "ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ"),
-    ("https://t.me/+ZyRZJntl2FU0NTk1", "ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ")
+    ("https://t.me/Bey_war_updates", ""),
+    ("https://t.me/+ZyRZJntl2FU0NTk1", "Support Group")
 ]
 
 
@@ -26,7 +27,7 @@ async def check_subscription(bot, message: Message):
             ])
             await message.reply(
                 f"›› ʜᴇʏ {message.from_user.mention} ×\n\n"
-                "```#JOIN ALL CHANNEL\nʏᴏᴜʀ ғɪʟᴇ ɪs ʀᴇᴀᴅʏ ‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ sᴜʙsᴄʀɪʙᴇᴅ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ,\n"
+                "```#JOIN CHANNEL\nʏᴏᴜʀ ғɪʟᴇ ɪs ʀᴇᴀᴅʏ ‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ sᴜʙsᴄʀɪʙᴇᴅ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ,\n"
                 "sᴜʙsᴄʀɪʙᴇ ɴᴏᴡ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ғɪʟᴇs.```",
                 reply_markup=buttons
             )
@@ -34,53 +35,47 @@ async def check_subscription(bot, message: Message):
     return True
 
 
-# Welcome video
-async def send_start_video(bot, chat_id, mention):
-    video_file_id = "BAACAgQAAxkBAAMHaHKBXy2VCMPrAAH8VcpV91M5lP9fAALnBwACiQ5tUWroh4Dwqk4rHgQ"
-    caption = (
-        f"🌟 {mention}, 𝕎𝕖𝕝𝕔𝕠𝕞𝕖 𝕥𝕠 𝕥𝕙𝕖 𝔽𝕚𝕝𝕖 𝕍𝕒𝕦𝕝𝕥!\n\n"
-        "⚡ 𝕐𝕠𝕦𝕣 𝕦𝕝𝕥𝕚𝕞𝕒𝕥𝕖 𝕙𝕚𝕕𝕖𝕠𝕦𝕥 𝕗𝕠𝕣 𝕡𝕖𝕣𝕞𝕒𝕟𝕖𝕟𝕥 𝕗𝕚𝕝𝕖 𝕤𝕥𝕠𝕣𝕒𝕘𝕖!\n\n"
-        "📂 𝕊𝕒𝕧𝕖 𝕗𝕚𝕝𝕖𝕤, 𝕘𝕣𝕒𝕓 𝕤𝕙𝕒𝕣𝕖𝕒𝕓𝕝𝕖 𝕝𝕚𝕟𝕜𝕤, 𝕒𝕟𝕕 𝕒𝕔𝕔𝕖𝕤𝕤 𝕥𝕙𝕖𝕞 𝕒𝕟𝕪𝕥𝕚𝕞𝕖.\n\n"
-        "📥 𝔻𝕣𝕠𝕡 𝕒 𝕗𝕚𝕝𝕖 𝕥𝕠 𝕦𝕟𝕝𝕖𝕒𝕤𝕙 𝕥𝕙𝕖 𝕡𝕠𝕨𝕖𝕣 𝕠𝕣 𝕙𝕚𝕥 /help 𝕗𝕠𝕣 𝕥𝕙𝕖 𝕗𝕦𝕝𝕝 𝕟𝕚𝕟𝕛𝕒 𝕤𝕔𝕣𝕠𝕝𝕝! 🚀"
-    )
-    buttons = InlineKeyboardMarkup([
-        [
-            InlineKeyboardButton("ʜᴇʟᴘ", callback_data="help_menu"),
-            InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="about_menu")
-        ],
-        [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ", url=CHANNELS[1][0])],
-        [InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ", url=CHANNELS[0][0])]
-    ])
-    await bot.send_video(chat_id, video_file_id, caption=caption, reply_markup=buttons)
+def generate_code(length=5):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 
-@bot.on_message(filters.command("start") & filters.private)
-async def start_command(bot, message: Message):
+@bot.on_message(filters.command("genlink") & filters.private)
+async def genlink_handler(bot, message: Message):
+    if message.from_user.id in Banned:
+        return await message.reply("🚫 You are banned from using this bot.")
+
     if not await check_subscription(bot, message):
         return
 
-    user = message.from_user
-    mention = user.mention
+    if not message.reply_to_message or not (
+        message.reply_to_message.video or message.reply_to_message.photo
+    ):
+        buttons = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ", url=CHANNELS[0][0])]]
+        )
+        return await message.reply(
+            "❗ Reply to a video or image to generate a link.",
+            reply_markup=buttons
+        )
 
-    await Users.update_one({"_id": user.id}, {"$set": {"name": user.first_name}}, upsert=True)
+    code = generate_code()
+    link = f"https://t.me/{BOT_USERNAME}?start={code}"
+    media = message.reply_to_message.video or message.reply_to_message.photo
+    file_id = media.file_id
+    caption = message.reply_to_message.caption
 
-    args = message.text.split(maxsplit=1)
-    if len(args) == 2:
-        code = args[1]
-        data = await file_collection.find_one({"code": code})
-        if data:
-            try:
-                file_type = data.get("type", "document")
-                file_id = data["file_id"]
-                caption = data.get("caption") or "📦 𝗙𝗶𝗹𝗲 𝗳𝗿𝗼𝗺 𝗟𝗶𝗻𝗸"
-                if file_type == "video":
-                    return await message.reply_video(video=file_id, caption=caption)
-                elif file_type == "photo":
-                    return await message.reply_photo(photo=file_id, caption=caption)
-                else:
-                    return await message.reply_document(document=file_id, caption=caption)
-            except Exception as e:
-                return await message.reply(f"❌ Error sending file:\n<code>{e}</code>")
-        return await message.reply("❗ Invalid or expired link.")
+    await file_collection.insert_one({
+        "code": code,
+        "file_id": file_id,
+        "caption": caption,
+        "type": "video" if message.reply_to_message.video else "photo"
+    })
 
-    await send_start_video(bot, message.chat.id, mention)
+    buttons = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ", url=CHANNELS[0][0])]]
+    )
+
+    await message.reply(
+        f"📎 Here's your file link:\n<code>{link}</code>",
+        reply_markup=buttons
+    )
