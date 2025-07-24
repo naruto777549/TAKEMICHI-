882bac4f61d67069f9b2ba46d1623db5754e5fce
naruto777549 <shekhikrar026@gmail.com>
2025-07-24 22:22:22 +0530

    Update __init__.py

--- revengers/__init__.py
+++ revengers/__init__.py
@@ -4,7 +4,7 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 bot = Client(
-    "BlackGameBot",
+    "All_in_one",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
