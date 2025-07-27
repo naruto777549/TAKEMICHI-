457523544239bfb0a03cc99f098c987c473dcca2
naruto777549 <shekhikrar026@gmail.com>
2025-07-27 10:01:49 +0530

    Update __init__.py

--- revengers/__init__.py
+++ revengers/__init__.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 start_time = datetime.utcnow()
+
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
@@ -7,6 +8,6 @@ bot = Client(
     "All_in_one",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-    parse_mode=HTML
+    bot_token=BOT_TOKEN,  
+    parse_mode="html"     
 )
