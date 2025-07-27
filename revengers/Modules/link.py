5ffc93d8b68bdab263b53cbc4ff3fb08e651314d
naruto777549 <shekhikrar026@gmail.com>
2025-07-27 16:57:07 +0530

    Update link.py

--- revengers/Modules/link.py
+++ revengers/Modules/link.py
@@ -31,7 +31,7 @@ async def link_command(bot, message: Message):
 from pyrogram import filters
 from pyrogram.types import Message
 from revengers import bot
-from db import get_chakra, remove_chakra
+from revengers.db import get_chakra, remove_chakra
 import re
 
 # Regex for Instagram and YouTube URLs
