b42a4feb14c487ce461a699794bcca9bccdc95a3
naruto777549 <shekhikrar026@gmail.com>
2025-08-05 08:34:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "21218274"))
-API_HASH = os.getenv("API_HASH", "3474a18b61897c672d315fb330edb213")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7956858405:AAH_uXebrWLkZagsWsCAeyFLD1EAuNfiVe4")
-
-ADMINS = list(map(int, os.getenv("ADMINS", "7576729648,6642049252").split(",")))
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMINS = list(map(int, os.getenv("ADMINS").split(",")))
